89a6ba1602f3a197ac9ed152ac5a3f1696861c73
hasanonik <jh8539615@gmail.com>
2024-01-30 15:50:01 +0600

    update-1

--- script.js
+++ script.js
@@ -47,3 +47,16 @@ function getItemActiveIndex() {
    const itemActiveIndex = itemsArray.indexOf(itemActive);
    return itemActiveIndex;
 }
+
+
+/*-----*/
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+/*-----*/
